d3af29868488425e1df035e3ddac279067acf202
Koroshs <period1313@gmail.com>
2019-08-24 19:56:20 +0430

    test commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
+//test commit
 function App() {
   return (
     <div>
